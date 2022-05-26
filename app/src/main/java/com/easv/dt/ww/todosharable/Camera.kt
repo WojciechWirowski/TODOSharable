9dc81c9ab38451e22bfc6488da2bffa6bc3aff5a
WojciechWirowski <71640545+WojciechWirowski@users.noreply.github.com>
2022-05-26 15:09:50 +0200

    camera comments

--- app/src/main/java/com/easv/dt/ww/todosharable/Camera.kt
+++ app/src/main/java/com/easv/dt/ww/todosharable/Camera.kt
@@ -7,14 +7,12 @@ import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
-import android.provider.Telephony
 import android.util.Log
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
-import com.easv.dt.ww.todosharable.data.listItem.ListItemRepository
 import kotlinx.android.synthetic.main.camera.*
 import java.io.File
 import java.text.SimpleDateFormat
@@ -22,18 +20,20 @@ import java.util.*
 
 class Camera : AppCompatActivity(){
 
-    private var uri: Uri? = null
     val TAG = "xyz"
-
     private var mFile: File? = null
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.camera)
+        //set on take image button listener
         btnByFile.setOnClickListener { takeImage() }
-        ListItemRepository.initialize(this)
     }
 
+    //runs after pressing button
+    //creates output file and puts it into phone storage
+    //opens camera MediaStore.ACTION_IMAGE_CAPTURE to able user to take photo
+    //overrides created file with made photo
     private fun takeImage() {
         mFile = getOutputMediaFile("CameraFiles")
 
@@ -50,7 +50,9 @@ class Camera : AppCompatActivity(){
             "${applicationId}.provider",
             mFile!!))
 
-        uri = FileProvider.getUriForFile(
+        //for future usage we can use this uri to display our image in app
+
+        val uri = FileProvider.getUriForFile(
             this,
             "${applicationId}.provider",
             mFile!!)
@@ -58,6 +60,7 @@ class Camera : AppCompatActivity(){
         fileCallback.launch(intent)
     }
 
+    //starts activity and expect to receive result code RESULT_OK setting imgView image for created file
     private val fileCallback = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
     ){ activityResult ->
@@ -67,12 +70,15 @@ class Camera : AppCompatActivity(){
         else handleOther(activityResult.resultCode)
     }
 
+    //method for displaying image
     @SuppressLint("SetTextI18n")
     private fun showImageFromFile(imgView: ImageView, f: File) {
         imgView.setImageURI(Uri.fromFile(f))
         imgView.setBackgroundColor(Color.RED)
     }
 
+    //getting created file
+    //if couldn't create file returns failing log
     @SuppressLint("SimpleDateFormat")
     private fun getOutputMediaFile(folder: String): File? {
         val mediaStorageDir = File(this.getExternalFilesDir(Environment.DIRECTORY_PICTURES), folder)
@@ -91,6 +97,7 @@ class Camera : AppCompatActivity(){
                 "_" + timeStamp + "." + postfix)
     }
 
+    //user information to show toast after canceling taking photo or error
     private fun handleOther(resultCode: Int) {
         if (resultCode == RESULT_CANCELED)
             Toast.makeText(this, "Canceled...", Toast.LENGTH_LONG).show()
